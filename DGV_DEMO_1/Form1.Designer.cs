75db3b76a3941cafa31fe68133feb254b76fd256
J.Cruz <jcruzupr@users.noreply.github.com>
2020-11-02 18:13:32 -0400

    Add project files.

---
+++ DGV_DEMO_1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DGV_DEMO_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoadData = new System.Windows.Forms.Button();
+            this.listBoxInput = new System.Windows.Forms.ListBox();
+            this.BtnSaveData = new System.Windows.Forms.Button();
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.dgvDemo = new System.Windows.Forms.DataGridView();
+            this.BtnDataToDGV = new System.Windows.Forms.Button();
+            this.BtnDataFromDGV = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnLoadData
+            // 
+            this.BtnLoadData.Location = new System.Drawing.Point(111, 21);
+            this.BtnLoadData.Name = "BtnLoadData";
+            this.BtnLoadData.Size = new System.Drawing.Size(142, 42);
+            this.BtnLoadData.TabIndex = 0;
+            this.BtnLoadData.Text = "Load data into ListBox";
+            this.BtnLoadData.UseVisualStyleBackColor = true;
+            this.BtnLoadData.Click += new System.EventHandler(this.BtnLoadData_Click);
+            // 
+            // listBoxInput
+            // 
+            this.listBoxInput.FormattingEnabled = true;
+            this.listBoxInput.Location = new System.Drawing.Point(12, 85);
+            this.listBoxInput.Name = "listBoxInput";
+            this.listBoxInput.Size = new System.Drawing.Size(356, 212);
+            this.listBoxInput.TabIndex = 1;
+            // 
+            // BtnSaveData
+            // 
+            this.BtnSaveData.Location = new System.Drawing.Point(101, 653);
+            this.BtnSaveData.Name = "BtnSaveData";
+            this.BtnSaveData.Size = new System.Drawing.Size(142, 44);
+            this.BtnSaveData.TabIndex = 2;
+            this.BtnSaveData.Text = "Save data from ListBox";
+            this.BtnSaveData.UseVisualStyleBackColor = true;
+            this.BtnSaveData.Click += new System.EventHandler(this.BtnSaveData_Click);
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.Location = new System.Drawing.Point(12, 454);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(356, 186);
+            this.listBoxOutput.TabIndex = 3;
+            // 
+            // dgvDemo
+            // 
+            this.dgvDemo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDemo.Location = new System.Drawing.Point(400, 133);
+            this.dgvDemo.Name = "dgvDemo";
+            this.dgvDemo.Size = new System.Drawing.Size(711, 394);
+            this.dgvDemo.TabIndex = 4;
+            // 
+            // BtnDataToDGV
+            // 
+            this.BtnDataToDGV.Location = new System.Drawing.Point(81, 313);
+            this.BtnDataToDGV.Name = "BtnDataToDGV";
+            this.BtnDataToDGV.Size = new System.Drawing.Size(215, 42);
+            this.BtnDataToDGV.TabIndex = 5;
+            this.BtnDataToDGV.Text = "Load above data into Data Grid View";
+            this.BtnDataToDGV.UseVisualStyleBackColor = true;
+            this.BtnDataToDGV.Click += new System.EventHandler(this.BtnDataToDGV_Click);
+            // 
+            // BtnDataFromDGV
+            // 
+            this.BtnDataFromDGV.Location = new System.Drawing.Point(81, 396);
+            this.BtnDataFromDGV.Name = "BtnDataFromDGV";
+            this.BtnDataFromDGV.Size = new System.Drawing.Size(215, 42);
+            this.BtnDataFromDGV.TabIndex = 6;
+            this.BtnDataFromDGV.Text = "Copy data from DGV into List Box below";
+            this.BtnDataFromDGV.UseVisualStyleBackColor = true;
+            this.BtnDataFromDGV.Click += new System.EventHandler(this.BtnDataFromDGV_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(719, 616);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 23);
+            this.BtnExit.TabIndex = 7;
+            this.BtnExit.Text = "EXIT";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 709);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnDataFromDGV);
+            this.Controls.Add(this.BtnDataToDGV);
+            this.Controls.Add(this.dgvDemo);
+            this.Controls.Add(this.listBoxOutput);
+            this.Controls.Add(this.BtnSaveData);
+            this.Controls.Add(this.listBoxInput);
+            this.Controls.Add(this.BtnLoadData);
+            this.Name = "Form1";
+            this.Text = "DGV Demo 1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoadData;
+        private System.Windows.Forms.ListBox listBoxInput;
+        private System.Windows.Forms.Button BtnSaveData;
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.DataGridView dgvDemo;
+        private System.Windows.Forms.Button BtnDataToDGV;
+        private System.Windows.Forms.Button BtnDataFromDGV;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ DGV_DEMO_1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DGV_DEMO_1
+{
+    public partial class Form1 : Form
+    {
+        // Class variables
+        string[,] dataArray = null;
+        int rows, columns;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadData_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            // Select input file
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    inputFile = File.OpenText(openFileDialog.FileName);
+
+                    // Read file and add data to ListBox, then
+                    // save data to a 2D Array named dataArray
+                    putDataInto2DArray(getAndDisplayData(ref inputFile));
+                    // Hint: putDataInto2DArray(datarows)
+
+                    // Close the file
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Operation Cancelled");
+            }
+
+        }
+
+        public string[] getAndDisplayData(ref StreamReader inputFile)
+        {
+            // Temporarily read all rows into an array 
+            string[] allLines = File.ReadAllLines(openFileDialog.FileName);
+
+            // rows is the same as the length/count of all lines
+            rows = allLines.Length;
+            // columns is the same as the number of tokens
+            columns = allLines[0].Split(',').Length;
+            MessageBox.Show("File has " + rows + " rows and " + columns + " columns");
+
+            // Clear Listbox
+            listBoxInput.Items.Clear();
+            // Read/save all rows & display each line in the listbox
+            // Declare array of "rows"size
+            string[] dataRows = new string[rows];
+            for (int i = 0; i < dataRows.Length; i++)
+            {
+                // Read each row/line
+                dataRows[i] = inputFile.ReadLine();
+                // Display the row/line -- a single string
+                listBoxInput.Items.Add(dataRows[i]);
+            }
+
+            // Return the ref to the data
+            return dataRows;
+        }
+
+
+        public void putDataInto2DArray(string[] dataRows)
+        {
+            dataArray = new string[rows, columns];
+            string[] dataElements = null;
+
+            // Loop & Process row by row
+            for (int i = 0; i < dataRows.Length; i++)
+            {
+                dataElements = dataRows[i].Split(',');
+                for (int j = 0; j < dataElements.Length; j++)
+                {
+                    dataArray[i, j] = dataElements[j];
+                }
+            }
+            //return dataArray;
+        }
+
+
+        public void arrayToDGV(string[,] dataArray)
+        {
+            //Clear all DGV rows
+            dgvDemo.Rows.Clear();
+
+            int dataRows = dataArray.GetLength(0); // Length of First Dimension 
+            int dataCols = dataArray.GetLength(1); // Length of Second Dimension
+
+            // Add columns with headers & properties from first row
+            string colHeader;
+            for (int i = 0; i < dataCols; i++)
+            {
+                colHeader = dataArray[0, i];
+                dgvDemo.Columns.Add(colHeader, colHeader);
+                //**dgvDemo.Columns[i].DataPropertyName = colHeader;
+                //**dgvDemo.Columns[i].HeaderText = colHeader;
+            }
+
+            // Assumes row=0 contains headers
+            // Populate rows with content beyond 
+            for (int row = 1; row < dataRows; row++)
+            {
+                DataGridViewRow dgvRow = new DataGridViewRow();
+                dgvRow.CreateCells(this.dgvDemo);
+
+                for (int col = 0; col < dataCols; col++)
+                {
+                    dgvRow.Cells[col].Value = dataArray[row, col];
+                }
+
+                this.dgvDemo.Rows.Add(dgvRow);
+            }
+        }
+
+        private void BtnDataToDGV_Click(object sender, EventArgs e)
+        {
+            // Create a DGV with the array data
+            arrayToDGV(dataArray);
+        }
+
+        private void BtnDataFromDGV_Click(object sender, EventArgs e)
+        {
+
+                string[,] saveArray = new string[dgvDemo.RowCount, dgvDemo.ColumnCount];
+                string[] saveRow = new string[dgvDemo.RowCount];
+                string[] rowElements = new string[dgvDemo.ColumnCount];
+                //MessageBox.Show("Rows: " + dgvDemo.RowCount); 
+                
+                // Get headers
+                for (int i = 0; i < dgvDemo.ColumnCount; i++)
+                {
+                    saveArray[0, i] = dgvDemo.Columns[i].HeaderText;
+                }
+
+                //DataGridViewRow row2 = dgvDemo.Rows[0];
+
+                for (int i = 0; i < dgvDemo.RowCount - 1; i++)
+                //foreach (DataGridViewRow row in dgvDemo.Rows)
+                {
+                    foreach (DataGridViewColumn col in dgvDemo.Columns)
+                    {
+                        saveArray[i + 1, col.Index] =
+                           dgvDemo.Rows[i].Cells[col.Index].FormattedValue.ToString().Trim();
+                    }
+                }
+
+                // Clear listbox
+                listBoxOutput.Items.Clear();
+
+                for (int i = 0; i < saveArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < saveArray.GetLength(1); j++)
+                    {
+                        rowElements[j] = saveArray[i, j];
+                    }
+
+                    saveRow[i] = String.Join(",", rowElements);
+                    listBoxOutput.Items.Add(saveRow[i]);
+                }
+        }
+
+
+        private void BtnSaveData_Click(object sender, EventArgs e)
+        {
+            // Edit the following path according to your environment
+            saveFileDialog.InitialDirectory = "C:\\Users\\Jose\\Desktop\\App_Data";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    StreamWriter myOutputFile;
+                    myOutputFile = File.CreateText(saveFileDialog.FileName);
+
+                    foreach (string s in listBoxOutput.Items)
+                    {
+                        myOutputFile.WriteLine(s);
+                    }
+
+                    myOutputFile.Close();
+
+                    MessageBox.Show("Data saved to file");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Operation Canceled");
+            }
+        }
+
+
+    private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
